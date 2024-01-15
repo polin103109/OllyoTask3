f470fdbc5794dd72d3cb333a62a53d253d64b247
polin103109 <sabrinpolinece18@gmail.com>
2024-01-15 12:57:34 +0600

    added a modal to modify segments

--- src/Components/DraftSpin.jsx
+++ src/Components/DraftSpin.jsx
@@ -1,22 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowAltCircleDown } from '@fortawesome/free-solid-svg-icons';
-import "../Styles/Draft.css"
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleDown } from "@fortawesome/free-solid-svg-icons";
+import Modal from "./Modal";
+import "../Styles/Draft.css";
 
 const PolygonGenerator = ({ polygonsData, setPolygonsData }) => {
-  const [inputSize, setInputSize] = useState('');
-  const [polygonColor, setPolygonColor] = useState('');
-  const [polygonText, setPolygonText] = useState('');
-  const [rotationValue, setRotationValue] = useState(Math.ceil(Math.random() * 3600));
- const addtoSpinWheel = () =>{
+  const [inputSize, setInputSize] = useState("");
+  const [polygonColor, setPolygonColor] = useState("");
+  const [polygonText, setPolygonText] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [rotationValue, setRotationValue] = useState(
+    Math.ceil(Math.random() * 3600)
+  );
+  const addtoSpinWheel = () => {
     const newPolygon = {
-        size: inputSize,
-        color: polygonColor,
-        text: polygonText,
-      };
-      console.log(newPolygon);
-      setPolygonsData([...polygonsData, newPolygon]);
-      console.log('Polygons Length:', polygonsData.length);
+      size: inputSize,
+      color: polygonColor,
+      text: polygonText,
+    };
+    setPolygonsData([...polygonsData, newPolygon]);
+    setIsModalOpen(false);
+  };
+ const handleSegments = ()=>{
+  setIsModalOpen(true);
  }
 
   const handleInputChange = (e) => {
@@ -31,29 +37,29 @@ const PolygonGenerator = ({ polygonsData, setPolygonsData }) => {
     setPolygonText(e.target.value);
   };
 
-//   useEffect(() => {
-//     setNumPolygons(5);
-//   }, [inputSize]);
+  //   useEffect(() => {
+  //     setNumPolygons(5);
+  //   }, [inputSize]);
 
-//   const calculatePolygonPosition = (index) => {
-//     const angle = (360 / numPolygons) * index;
-//     const radius = 1000;
+  //   const calculatePolygonPosition = (index) => {
+  //     const angle = (360 / numPolygons) * index;
+  //     const radius = 1000;
 
-//     const x = radius * Math.cos((angle * Math.PI) / 180);
-//     const y = radius * Math.sin((angle * Math.PI) / 180);
+  //     const x = radius * Math.cos((angle * Math.PI) / 180);
+  //     const y = radius * Math.sin((angle * Math.PI) / 180);
 
-//     return { x, y };
-//   };
+  //     return { x, y };
+  //   };
 
   const polygons = polygonsData.map((polygon, index) => {
     // const { x, y } = calculatePolygonPosition(index);
 
     const polygonStyles = {
       //float: 'left',
-      '--i': index,
+      "--i": index,
       width: `${polygon.size}px`,
       height: `${polygon.size}px`,
-      position:'absolute',
+      position: "absolute",
       clipPath: `polygon(
        0 0,
        56% 0,
@@ -61,62 +67,78 @@ const PolygonGenerator = ({ polygonsData, setPolygonsData }) => {
        0 56%
       )`,
       background: polygon.color,
-      transformOrigin: 'bottom right',
-      display: 'flex',
-      alignItems: 'center', 
-      justifyContent: 'center',
+      transformOrigin: "bottom right",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
       transform: `rotate(${36 * index}deg)`,
-  }
-  
+    };
+
     return (
       <div key={index} style={polygonStyles}>
-        <p><span>{polygon.text}</span></p>
+        <p>
+          <span>{polygon.text}</span>
+        </p>
       </div>
     );
   });
   const playSpin = () => {
     const newValue = rotationValue + Math.ceil(Math.random() * 3600);
     setRotationValue(newValue);
-  }
+  };
 
   return (
-    <div className='spinFormContainer'>
-        <div className='inputDiv'>
-      <label htmlFor="inputSize">Enter size:</label>
-      <input
-        type="number"
-        id="inputSize"
-        value={inputSize}
-        onChange={handleInputChange}
-      />
-
-      <label htmlFor="polygonColor">Enter color:</label>
-      <input
-        type="text"
-        id="polygonColor"
-        value={polygonColor}
-        onChange={handleColorChange}
-      />
-
-      <label htmlFor="polygonText">Enter text:</label>
-      <input
-        type="text"
-        id="polygonText"
-        value={polygonText}
-        onChange={handleTextChange}
-      />
-      <button onClick={addtoSpinWheel}>ADD</button>
+    <div className="spinFormContainer">
+      <div className="inputDiv">
+        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+          <div>
+            <label htmlFor="inputSize">Enter size:</label>
+            <input
+              type="number"
+              id="inputSize"
+              value={inputSize}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="polygonColor">Enter color:</label>
+            <input
+              type="text"
+              id="polygonColor"
+              value={polygonColor}
+              onChange={handleColorChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="polygonText">Enter text:</label>
+            <input
+              type="text"
+              id="polygonText"
+              value={polygonText}
+              onChange={handleTextChange}
+            />
+          </div>
+          <div className="modalbottom"></div>
+          <button className="modalbutton" onClick={addtoSpinWheel}>
+            ADD
+          </button>
+        </Modal>
       </div>
-      <div className='wheelCircleContainer'>
-      <button className='spinButton' onClick={playSpin}>Spin to Win!</button>
-      <div className='wheelCircle'  style={{ transform: `rotate(${rotationValue}deg)`}}>
-        {polygons}
+      <div className="wheelCircleContainer">
+        <button className="segmentbutton" onClick={handleSegments}>Modify Segments</button>
+        <button className="spinButton" onClick={playSpin}>
+          Spin to Win!
+        </button>
+        <div
+          className="wheelCircle"
+          style={{ transform: `rotate(${rotationValue}deg)` }}
+        >
+          {polygons}
         </div>
         <div className="indicator">
-        <FontAwesomeIcon className='arrowicon' icon={faArrowAltCircleDown} />
+          <FontAwesomeIcon className="arrowicon" icon={faArrowAltCircleDown} />
         </div>
       </div>
-     
     </div>
   );
 };
